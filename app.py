54e68367ef1992addb3869bc3e229359fd3daeb1
Ashi Friedberg <ashifriedberg@gmail.com>
2024-01-02 00:54:14 +0200

    bug in spellchecker

--- app.py
+++ app.py
@@ -1,46 +1,57 @@
-import requests
+from bs4 import BeautifulSoup as bs
+from hebrew import Hebrew
 import json
-from bs4 import BeautifulSoup as bs 
+import requests
+import spellchecker 
 
-def extract_hebrew_text(loaded_json):
-        """Extract Hebrew text from loaded JSON while parsing HTML content.
 
-        Args:
-        - loaded_json: dict, the JSON object loaded from the request response
-
-        Returns:
-        - str, the Hebrew text with HTML tags removed
-        """
-        hebrew_html = loaded_json
-        soup = bs(hebrew_html, 'html.parser')
-        return soup.get_text()[::-1]
-
-# In main function, call the new extract_hebrew_text function
 def main():
+    """
+    Retrieves and displays the text of a book from the Sefaria API based on user input.
+
+    Returns:
+    None
+    """
     base_url = "https://www.sefaria.org/api/texts/"
-    # Add Hebrew book name as parameter
+    search = input("Search? y/n: ")
+    if search == "y":
+        find_text()
     book = input("Enter book name in english: ")
     response = requests.get(base_url + book)
     loaded_json = json.loads(response.text)
-    hebrew_sentences = []
-    for line in loaded_json['he']:
-        hebrew_sentences.append(extract_hebrew_text(line))
-    hebrew_paragraph = '\n'.join(hebrew_sentences)
-    print(hebrew_paragraph)
+    lang = input("Enter language: ")
+    if lang == "en":
+        english_sentences = []
+        for line in loaded_json["text"]:
+            english_sentences.append(bs(line, "html.parser").get_text())
+        english_paragraph = "\n".join(english_sentences)
+        print(english_paragraph)
+    elif lang == "he":
+        hebrew_sentences = []
+        for line in loaded_json["he"]:
+            hebrew_sentences.append(bs(line, "html.parser").get_text())
+        hebrew_paragraph = "\n".join(hebrew_sentences)
+        clean = Hebrew(hebrew_paragraph)
+        print(clean.text_only())
 
-def extract_hebrew_text(loaded_json):
-    """Extract Hebrew text from loaded JSON while parsing HTML content.
+def find_text():
+    response = requests.get("https://www.sefaria.org/api/index/titles")
+    all_titles = json.loads(response.text)
+    search = input("what book are you looking for? ")
 
-    Args:
-    - loaded_json: dict, the JSON object loaded from the request response
+    # Create a SpellChecker instance
+    spell = spellchecker.Spellchecker()
 
-    Returns:
-    - str, the Hebrew text with HTML tags removed
-    """
-    hebrew_html = loaded_json
-    soup = bs(hebrew_html, 'html.parser')
-    return soup.get_text()[::-1]
+    # Load the words into the spell checker
+    spell.word_frequency.load_words(all_titles)
+
+    # Perform spell checking
+    misspelled = spell.unknown(search)
+
+    # Get the corrected version of misspelled words
+    corrected = [spell.correction(word) for word in misspelled]
+
+    print(f"are you looking for {corrected}?") 
 
 
 main()
-
