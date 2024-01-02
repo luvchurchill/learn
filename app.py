a680eb966bdc54335eec6d2e5f88a095ec26dcd2
Ashi Friedberg <ashifriedberg@gmail.com>
2024-01-03 00:06:39 +0200

    feat: interactive mode

--- app.py
+++ app.py
@@ -1,34 +1,23 @@
 import argparse
 from bs4 import BeautifulSoup as bs
-import difflib
 from hebrew import Hebrew
 import json
+import pyfiglet
 import requests
-
-
-class SpellChecker:
-    def __init__(self, wordlist):
-        self.wordlist = wordlist
-
-    def find_similar_word(self, word):
-        """
-        Find similar words in the wordlist based on a given word.
-
-        Parameters:
-            word (str): The word to find similar matches for.
-
-        Returns:
-            List[str]: A list of up to 3 similar words from the wordlist.
-        """
-        return difflib.get_close_matches(word, self.wordlist, n=3)
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Search and retrieve Jewish texts.")
     parser.add_argument(
-    "-s", "--search", type=str, nargs='+', help="search for the correct spelling of the book"
-)
-    parser.add_argument("-b", "--book", type=str, nargs='+', help="Enter book name in English")
+        "-s",
+        "--search",
+        type=str,
+        nargs="+",
+        help="search for the correct spelling of the book",
+    )
+    parser.add_argument(
+        "-b", "--book", type=str, nargs="+", help="Enter book name in English"
+    )
     parser.add_argument(
         "-l",
         "--language",
@@ -36,18 +25,23 @@ def parse_arguments():
         choices=["en", "he"],
         help="Choose language of the text",
     )
+    parser.add_argument(
+        "-i", "--interactive", action="store_true", help="Use the program interactively"
+    )
     return parser.parse_args()
 
 
 def main():
     args = parse_arguments()
 
-    if args.search:
+    if args.interactive:
+        interactive_mode()
+    elif args.search:
         find_book(args.search)
     elif args.book and args.language:
         retrieve_text(args.book, args.language)
     else:
-        print("Invalid arguments. Please use -h or --help for help.")
+        print("Invalid arguments. Use -h or --help for more information.")
 
 
 def retrieve_text(book, language):
@@ -59,7 +53,10 @@ def retrieve_text(book, language):
     :param language: The language of the text to retrieve ("en" for English, "he" for Hebrew).
     :type language: str
     """
-    book_name = " ".join(book)
+    if type(book) == list:
+        book_name = " ".join(book)
+    else:
+        book_name = book
     base_url = "https://www.sefaria.org/api/texts/"
     response = requests.get(base_url + book_name)
     loaded_json = json.loads(response.text)
@@ -78,8 +75,12 @@ def retrieve_text(book, language):
             clean = Hebrew(hebrew_paragraph)
             text_only = clean.text_only()
             print(text_only)
+        else:
+            print("Invalid language. Please use 'en' or 'he'.")
     except KeyError:
-        print("The text was not found. use the search function to find the correct spelling")
+        print(
+            "The text was not found. use the search function to find the correct spelling"
+        )
 
 
 def find_book(search):
@@ -95,10 +96,41 @@ def find_book(search):
     searched = " ".join(search)
     response = requests.get("https://www.sefaria.org/api/name/" + searched)
     loaded_json = json.loads(response.text)
-    if loaded_json['is_ref'] == True:
+    if loaded_json["is_ref"] == True:
         print("It seems like you have the correct spelling")
     else:
-        print(loaded_json['completions'])
+        print(loaded_json["completions"])
+
+
+def interactive_mode():
+    name = "Learn"
+    tagline = "Like a hacker"
+    print(pyfiglet.figlet_format(name))
+    print(pyfiglet.figlet_format(tagline, font="univers"))
+    print("Do you know the correct spelling of the book? (y/n)")
+    needs_search = input(">>> ")
+    if needs_search == "n":
+        help_searching()
+    elif needs_search == "y":
+        print("Enter the name of the book: ")
+        book = input(">>> ")
+        print(
+            "Enter the language you want to read in ('en' for English, 'he' for Hebrew): "
+        )
+        language = input(">>> ")
+        retrieve_text(book, language)
+
+
+def help_searching():
+    print("Enter a possible spelling: ")
+    search = input(">>> ")
+    print("Here are possible matches: ")
+    find_book(search)
+    print("did you find what you're looking for? (y/n)")
+    found = input(">>> ")
+    if found == "n":
+        help_searching()
+
 
 if __name__ == "__main__":
     main()
